84a549fb1e13b11b5cb1057b4739918d2dfc0219
Mutamimahtik3 <54967154+Mutamimahtik3@users.noreply.github.com>
2020-12-01 12:55:09 +0700

    Delete index.blade.php

--- resources/views/admin/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-<div class="section-body">
-    <div class="alert alert-success">Selamat datang, {{ Auth::user()->name }}. Halaman Dashboard Admin</div>
-</div>
-@endsection
