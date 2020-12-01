6d0b0e963c37fa85b11e959c8a399d7c8c6a4e95
Mutamimahtik3 <54967154+Mutamimahtik3@users.noreply.github.com>
2020-12-01 12:54:14 +0700

    Delete index.blade.php

--- resources/views/petani/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-<div class="section-body">
-    <div class="alert alert-success">Selamat datang, {{ Auth::user()->name }}. Halaman Dashboard Petani</div>
-</div>
-@endsection
